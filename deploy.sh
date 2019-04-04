edf41261b460ad07872db9db3bc651ef3af90642
Nick Golding <goldingn35@gmail.com>
2019-04-04 17:56:33 +1100

    add deployment via a submodule

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# build and deploy the website, following instructions from here: https://gohugo.io/hosting-and-deployment/hosting-on-github/
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
